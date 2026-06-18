6dbe706e42856ad65e375e6514ae5c424edcae0b
Jonathan Cidierdes Caitano Lima <1167794@sga.pucminas.br>
2026-06-18 19:52:57 -0300

    Inicio da parte 2 do projeto

--- Trabalho Prático Final/Trabalho Prático Final/Program.cs
+++ Trabalho Prático Final/Trabalho Prático Final/Program.cs
@@ -1,37 +1,129 @@
-﻿class Program
+﻿using System;
+using System.IO;
+
+class Program
 {
-    public int tamanhoLista = 0;
+    public static Aluno[] listaDeAlunos = new Aluno[100];
+    public static int tamanhoLista = 0;
+    public static string nomeArquivo = "Arquivo de Alunos";
+
     static void Main()
     {
-        Aluno[] listaDeAlunos = new Aluno[100];
-        
-        Console.WriteLine("Digite a operação desejada:\n0 – Sair do programa\n1 - Cadastrar um aluno\n2 – Listar todos os alunos");
-        int digito = int.Parse(Console.ReadLine());
+        int opt = menu();
 
-        while (digito != 0)
+        while (opt != 0)
         {
-            switch (digito)
+            switch (opt)
             {
                 case 0:
-                    Console.WriteLine("Saindo do programa...");
                     break;
                 case 1:
-                    novoAluno();
+                    salvarArquivo();
                     break;
                 case 2:
+                    carregarArquivo();
+                    break;
+                case 3:
+                    novoAluno();
+                    break;
+                case 4:
                     listarAlunos();
                     break;
+                case 5:
+                    pesquisarPeloNome();
+                    break;
+                case 6:
+                    listarAniversariantes();
+                    break;
+                case 7:
+                    excluirPorCpf();
+                    break;
+                case 8:
+                    excluirTodosAlunos();
+                    break;
                 default:
-                    Console.WriteLine("Valor inválido!");
+                    Console.WriteLine("\n!!! Opção inválida !!!");
                     break;
             }
+            opt = menu();
         }
+        Console.WriteLine("\n\n[Saindo do programa...]");
     }
-    void novoAluno()
+
+    public static int menu()
+    {
+        Console.Write("\n\n============================\n" +
+            "||   Registro de Alunos   ||\n" +
+            "============================\n" +
+            "Digite a operação desejada:\n\n" +
+            "0 – Sair do programa\n" +
+            "1 – Salvar os dados dos alunos em um arquivo\n" +
+            "2 – Recuperar os dados dos alunos armazenados em um arquivo\n" +
+            "3 - Cadastrar um Aluno\n" +
+            "4 – Listar todos os alunos\n" +
+            "5 – Pesquisar alunos por um nome chave de pesquisa\n" +
+            "6 – Listar os alunos aniversariantes de um mês chave de pesquisa\n" +
+            "7 – Excluir um aluno a partir de um CPF chave de pesquisa\n" +
+            "8 – Excluir todos os alunos\n" +
+            "\n -> ");
+        return int.Parse(Console.ReadLine());
+    }
+    public static void salvarArquivo()
+    {
+        StreamWriter escritor = new StreamWriter(nomeArquivo);
+        for (int i = 0; i < tamanhoLista; i++)
+        {
+            escritor.WriteLine(listaDeAlunos[i]);
+        }
+        escritor.Close();
+
+        Console.WriteLine("\n--- Arquvo salvo com sucesso! ---");
+    }
+    public static void carregarArquivo()
+    {
+        StreamReader leitor = new StreamReader(nomeArquivo);
+        for (int i = 0; i < tamanhoLista; i++)
+        {
+            listaDeAlunos[i] = leitor.ReadLine();
+        }
+        leitor.Close();
+        Console.WriteLine("\n--- Arquvo carregado com sucesso! ---");
+    }
+    public static void novoAluno()
+    {
+        Console.WriteLine($"\n» Cadastro do {tamanhoLista + 1}° aluno: ");
+        Aluno novoAluno = new Aluno();
+        novoAluno.leiaAluno();
+        listaDeAlunos[tamanhoLista] = novoAluno;
+        tamanhoLista++;
+    }
+    public static void listarAlunos()
+    {
+        if (tamanhoLista > 0)
+        {
+            Console.WriteLine("\n» Lista de Alunos registrados:");
+            for (int i = 0; i < tamanhoLista; i++)
+            {
+                listaDeAlunos[i].escrevaAluno();
+                Console.WriteLine();
+            }
+        }
+        else Console.WriteLine("\n--- Nenhum aluno foi registrado! ---");
+
+    }
+    public static void pesquisarPeloNome()
     {
 
     }
-    void listarAlunos()
+    public static void listarAniversariantes()
+    {
+
+    }
+    public static void excluirPorCpf()
+    {
+
+    }
+    public static void excluirTodosAlunos()
     {
 
     }
@@ -40,7 +132,10 @@
 class Aluno
 {
     private string nome;
-    private Data nascimento;
+    private string cpf;
+    private Data nascimento = new Data();
+    private Data cadastro = new Data();
+    //private double nota = 0;
     public static int qtAlunos = 0;
 
     public Aluno() { qtAlunos++; }
@@ -48,6 +143,7 @@ class Aluno
     {
         setNome(nome);
         nascimento.setData(dia, mes, ano);
+        setCadastro(dia, mes, ano);
         qtAlunos++;
     }
     public void setNome(string nome)
@@ -58,6 +154,7 @@ class Aluno
     {
         return nome;
     }
+
     public bool setNascimento(int dia, int mes, int ano)
     {
         nascimento.setData(dia, mes, ano);
@@ -67,26 +164,57 @@ class Aluno
     {
         return nascimento;
     }
+
+    public void setCadastro(int dia,int mes,int ano)
+    {
+        cadastro.setData(dia, mes, ano);
+    }
+    public Data getCadastro()
+    {
+        return cadastro;
+    }
+
+    public void setCpf(string cpf)
+    {
+        this.cpf = cpf;
+    }
+    public string getCpf()
+    {
+        return cpf;
+    }
+
     public void leiaNome()
     {
-        Console.WriteLine("Digite o nome:");
+        Console.Write("\nDigite o nome do aluno: ");
         setNome(Console.ReadLine());
     }
     public void escrevaNome()
     {
         Console.Write(getNome());
     }
+
     public void leiaAluno()
     {
         leiaNome();
-        Console.WriteLine("\nPara inserir a data de nascimento:");
+        Console.WriteLine("Digite o CPF: ");
+        setCpf(Console.ReadLine());
+        Console.WriteLine("\nPara inserir a data de nascimento...");
         nascimento.leiaData();
+        Console.WriteLine("\nPara inserir a data de cadastro...");
+        cadastro.leiaData();
+        Console.WriteLine("\n=== Aluno cadastrado com sucesso! ===");
     }
     public void escrevaAluno()
     {
+        Console.Write("Nome: ");
         escrevaNome();
-        Console.WriteLine("Nascimento: ");
+        Console.Write("\nNascimento: ");
         nascimento.escrevaData();
+        Console.Write("\nCadastro: ");
+        cadastro.escrevaData();
+        Console.Write("\nCPF: ");
+        Console.WriteLine(getCpf());
+        Console.WriteLine();
     }
 }
 
@@ -118,6 +246,7 @@ class Data
     {
         this.ano = ano;
     }
+
     public bool setData(int dia, int mes, int ano)
     {
         setDia(dia);
@@ -125,6 +254,7 @@ class Data
         setAno(ano);
         return dataValida();
     }
+
     public int getDia()
     {
         return dia;
@@ -137,6 +267,7 @@ class Data
     {
         return ano;
     }
+
     public bool dataValida()
     {
         bool valida = false;
@@ -145,7 +276,7 @@ class Data
     }
     public string mesExtenso()
     {
-        string[] nomeMes = { "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" }
+        string[] nomeMes = { "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro" };
         return nomeMes[getMes() - 1];
     }
     public int diasMes()
@@ -155,17 +286,18 @@ class Data
             qtDias[1] = 29;
         return qtDias[getMes() - 1];
     }
+
     public void escrevaData()
     {
         Console.Write(getDia() + "/" + getMes() + "/" + getAno());
     }
     public void leiaData()
     {
-        Console.WriteLine("Digite o dia:");
+        Console.Write("Digite o dia: ");
         setDia(int.Parse(Console.ReadLine()));
-        Console.WriteLine("Digite o mês:");
+        Console.Write("Digite o mês:" );
         setMes(int.Parse(Console.ReadLine()));
-        Console.WriteLine("Digite o ano:");
+        Console.Write("Digite o ano:" );
         setAno(int.Parse(Console.ReadLine()));
     }
 }
